bd7e1a734ab6ec315e6c66641bdb327c82fbbcaf
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-23 18:55:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -13,5 +15,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
